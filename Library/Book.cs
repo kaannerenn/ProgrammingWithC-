26315473627ffdf99210047513acd9be26f0175e
kaannerenn <yavuzk20@itu.edu.tr>
2025-02-23 20:16:32 +0300

    Code practice files

---
+++ Library/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PublicationYear { get; set; }
+        public string Type { get; set; }
+        public int PageCount { get; set; }
+        public decimal Cost { get; set; }
+        public string Description { get; set; }
+
+        
+    }
+}
---
+++ Library/Program.cs
@@ -0,0 +1,111 @@
+﻿using Newtonsoft.Json;
+using Library;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http.Json;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace Library
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter your customer type.");
+            Console.WriteLine("Normal(N)\nStudent(S)\nVeteran(V)");
+            var userInput = Console.ReadLine();
+            userInput = userInput.ToUpper();
+            Console.WriteLine("Which book do you want to view?");
+            string filePath = "BooksInfo.json";
+            List<Book> books = new List<Book>();
+            if (File.Exists(filePath))
+            {
+                string jsonContent = File.ReadAllText(filePath);
+
+                books = JsonConvert.DeserializeObject<List<Book>>(jsonContent);
+            }
+            if (File.Exists(filePath))
+            {
+               
+                string jsonContent = File.ReadAllText(filePath);
+
+                
+                books = JsonConvert.DeserializeObject<List<Book>>(jsonContent);
+
+                
+                if (books != null)
+                {
+                    foreach (var book in books)
+                    {
+                        Console.WriteLine(book.Title);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No books found in the JSON.");
+                }
+            }
+                else
+                {
+                    Console.WriteLine("BooksInfo.json file does not exist.");
+                }
+            var userInput2 = Console.ReadLine();
+            var selectedBook = books.Find(b => b.Title.Equals(userInput2, StringComparison.OrdinalIgnoreCase));
+            if(selectedBook != null)
+            {
+                Console.WriteLine("\nDetails of the book which you choose");
+                Console.WriteLine($"Title: {selectedBook.Title}");
+                Console.WriteLine($"Author: {selectedBook.Author}");
+                Console.WriteLine($"Year: {selectedBook.PublicationYear}");
+                Console.WriteLine($"Type: {selectedBook.Type}");
+                Console.WriteLine($"Page Count: {selectedBook.PageCount}");
+                Console.WriteLine($"Cost: {selectedBook.Cost}$");
+                Console.WriteLine($"Description: {selectedBook.Description}");
+            }
+            else
+            {
+                Console.WriteLine("The book which you choose does not exist.");
+            }
+
+             Console.WriteLine("Do you want to buy this book? If yes press (B), else press (X)");
+             var userInput3 = Console.ReadLine();            
+             userInput3 = userInput3.ToUpper();
+             if(userInput3 == "B")
+             {
+                if(userInput == "N")
+                {
+                    Console.WriteLine("If you have normal membership you have no discount");
+                    var price = selectedBook.Cost;
+                    Console.WriteLine("The price does not changed. Price is = {}", price);
+                }
+                else if(userInput == "S")
+                {
+                    Console.WriteLine("If you have student membership you have %20 discount");
+                    var price = selectedBook.Cost * 80 / 100;
+                    Console.WriteLine("The new price is {0}",price);
+                }
+                else if(userInput == "V")
+                {
+                    Console.WriteLine("If you have veteran membership you have %10 discount");
+                    var price = selectedBook.Cost * 90 / 100;
+                    Console.WriteLine("The new price is {0}", price);
+                }
+                else
+                {
+                    Console.WriteLine("The membership type does not exist.");
+                }
+             }
+            else if(userInput3 == "X")
+            {
+                Console.WriteLine("Returning to menu.");
+
+            }
+            else
+            {
+                Console.WriteLine("You pressed an invalid key.");
+            }
+        }
+    } 
+}
+
